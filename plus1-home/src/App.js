79de6175d8a09d9dc2f9b9aa027562a14fd815e7
runninginair <runninginair@gmail.com>
2023-04-10 22:50:40 -0700

    bug fixed

--- plus1-home/src/App.js
+++ plus1-home/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import Labs from './labs';
-import HelloWorld from './labs/a6/hello-world';
+// import Labs from './labs';
+// import HelloWorld from './labs/a6/hello-world';
 import Tuiter from './tuiter';
 import { BrowserRouter, Link } from 'react-router-dom';
 import { Routes, Route } from 'react-router';
-import TodosthList from './screens/todosth/todosth-list';
-import ProfileScreenDemo from './screens/profile-demo/profile-screen-demo';
+// import ProfileScreenDemo from './screens/profile-demo/profile-screen-demo';
 import { Provider } from 'react-redux';
 import store from './tuiter/data/store';
-import HomScreenDemo from './screens/profile-demo/home-screen';
+// import HomScreenDemo from './screens/profile-demo/home-screen';
 import './App.css';
-
 
 function App() {
   return (
@@ -19,13 +17,8 @@ function App() {
         <div className="container">
 
           <Routes>
-            <Route path="/*" element={<Labs />} />
-            <Route path="/userlogin" element={<HelloWorld />} />
-            <Route path="/plus1/*" element={<Tuiter />} />
 
-            {/* <Route path="/todosth" element={<TodosthList />} /> */}
-            <Route path="/plus1-home" element={<HomScreenDemo />} />
-            <Route path="/profile-demo" element={<ProfileScreenDemo />} />
+            <Route path="/plus1/*" element={<Tuiter />} />
 
           </Routes>
         </div>
